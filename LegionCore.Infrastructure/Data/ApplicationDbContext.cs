abdcda3bab1bab28d40ce867643af121e9dd5b9c
Vincent Lauro <vinniehat@gmail.com>
2022-07-24 12:03:40 -0400

    Changed ApplicationDBContext Database Location

--- LegionCore.Infrastructure/Data/ApplicationDbContext.cs
+++ LegionCore.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LegionCore.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LegionCore.Infrastructure.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
