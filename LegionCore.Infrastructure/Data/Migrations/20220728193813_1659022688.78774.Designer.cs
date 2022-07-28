9e7a2bd287b97fe1ae3f99954ca208ac5776f018
Vincent Lauro <vinniehat@gmail.com>
2022-07-28 15:39:14 -0400

    Added new ApplicationUser/Role migration

--- LegionCore.Infrastructure/Data/Migrations/20220728193813_1659022688.78774.Designer.cs
+++ LegionCore.Infrastructure/Data/Migrations/20220728193813_1659022688.78774.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LegionCore.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LegionCore.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220728193813_1659022688.78774")]
+    partial class _165902268878774
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.7");
---
+++ LegionCore.Infrastructure/Data/Migrations/20220728193813_1659022688.78774.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LegionCore.Web.Data.Migrations
+{
+    public partial class _165902268878774 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetRoles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "AspNetRoles",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "AspNetRoles");
+        }
+    }
+}
